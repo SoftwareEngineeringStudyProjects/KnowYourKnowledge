0d01a1acbf068445c5a34d778df37beb3e7f1304
Kostiantyn Zhereb <zhereb@gmail.com>
2022-09-20 12:03:21 +0300

    Implemented unit test for stream output
    It also checks output to other streams - such as files, or std::cout
    Test may fail, because output order from std::unordered_stream is not
    deterministic

--- src/cpp/Config.cpp
+++ src/cpp/Config.cpp
@@ -10,6 +10,7 @@
 #include "doctest.h"
 
 #include <fstream>
+#include <sstream>
 
 Config::Config() {
 	// TODO Auto-generated constructor stub
@@ -69,4 +70,18 @@ TEST_CASE("Get non-existing key should throw exception") {
 	Config config;
 	config.set("current_collection", "MyNotes");
 	CHECK_THROWS_AS(config.get("non_existing"), std::out_of_range);
+}
+
+TEST_CASE("Output config to stream") {
+	Config config;
+
+    config.set("current_collection", "Another with space");
+    CHECK(config.get("current_collection") == "Another with space");
+    config.set("something","anything");
+    CHECK(config.get("something") == "anything");
+
+    std::stringstream out;
+    out<<config;
+    CHECK(out.str() == "something=anything\ncurrent_collection=Another with space\n");
+
 }
