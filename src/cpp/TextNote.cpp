836f6486258fbef3747d0f732b9e7f32dacd3234
Kostiantyn Zhereb <zhereb@gmail.com>
2022-09-22 15:40:58 +0300

    Implemented unit test for TextNote
    Will be used to check transition to base class
    Also fixes #20

--- src/cpp/TextNote.cpp
+++ src/cpp/TextNote.cpp
@@ -7,6 +7,8 @@
 
 #include "TextNote.h"
 
+#include "doctest.h"
+
 #include <cassert>
 
 TextNote::TextNote(std::string title, std::string text): _title{title}, _text{text} {
@@ -27,6 +29,10 @@ std::string TextNote::text() const {
 	return _text;
 }
 
+std::time_t TextNote::creation_time() const{
+	return _creation_time;
+}
+
 std::string TextNote::creation_time_string() const {
 	return std::asctime(std::localtime(&_creation_time));
 }
@@ -34,3 +40,15 @@ std::string TextNote::creation_time_string() const {
 void TextNote::print() const {
 	std::cout<<"title="<<title()<<", text="<<text()<<",created="<<creation_time_string()<<std::endl;
 }
+
+TEST_CASE("creating object with fixed creation time") {
+	//1663850154 = Thu Sep 22 15:35:54 2022
+	TextNote note("hello","this is text\ntext\n",1663850154);
+
+	CHECK(note.title() == "hello");
+	CHECK(note.text() == "this is text\ntext\n");
+	CHECK(note.creation_time() == 1663850154);
+	CHECK(note.creation_time_string() == "Thu Sep 22 15:35:54 2022\n");
+
+}
+
