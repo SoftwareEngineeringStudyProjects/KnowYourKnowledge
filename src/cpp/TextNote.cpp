3e00b67a99dbf282fb53a66a59c774c0a91d8bc6
Kostiantyn Zhereb <zhereb@gmail.com>
2022-09-23 13:18:48 +0300

    Make TextNote inherit from KnowledgeItem
    Removes code duplication
    Also fixes #20 (and this is better approach, because don't need to
    remember this in each subclass)

--- src/cpp/TextNote.cpp
+++ src/cpp/TextNote.cpp
@@ -11,30 +11,14 @@
 
 #include <cassert>
 
-TextNote::TextNote(std::string title, std::string text): _title{title}, _text{text} {
-	assert(title.length() != 0);
-	_creation_time = std::time(nullptr);
-
+TextNote::TextNote(std::string title, std::string text): KnowledgeItem{title}, _text{text} {
 }
 
 TextNote::TextNote(std::string title, std::string text, std::time_t creation_time):
-		_title(title), _text(text), _creation_time(creation_time){}
-
-
-std::string TextNote::title() const {
-	return _title;
-}
+		KnowledgeItem{title, creation_time}, _text{text}{}
 
 std::string TextNote::text() const {
 	return _text;
-}
-
-std::time_t TextNote::creation_time() const{
-	return _creation_time;
-}
-
-std::string TextNote::creation_time_string() const {
-	return std::asctime(std::localtime(&_creation_time));
 }
 
 void TextNote::print() const {
