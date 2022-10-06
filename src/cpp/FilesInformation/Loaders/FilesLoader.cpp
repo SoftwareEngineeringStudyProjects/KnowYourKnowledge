71c5d45c935d049884b23ce3da8280312558a5a7
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-10-06 20:24:04 +0300

    Added logic to handle case when there is no file

--- src/cpp/FilesInformation/Loaders/FilesLoader.cpp
+++ src/cpp/FilesInformation/Loaders/FilesLoader.cpp
@@ -6,6 +6,7 @@
 #include "../../doctest.h"
 
 Directory *FilesLoader::loadDirectory(const std::string &filepath) {
+    if (!std::filesystem::exists(filepath)) return nullptr;
     Directory *directory = new Directory(filepath);
     for (const auto &entry: std::filesystem::directory_iterator(filepath)) {
         if (entry.is_directory()) {
