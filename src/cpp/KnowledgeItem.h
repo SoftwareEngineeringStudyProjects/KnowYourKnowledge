bc5f9bbfecd3337da1ea45d7bc0d79b5a702c3d9
RyanGoslenko <olkhovatyi@knu.ua>
2022-10-11 11:17:41 +0300

    Knowledgeitem

--- src/cpp/KnowledgeItem.h
+++ src/cpp/KnowledgeItem.h
@@ -8,34 +8,35 @@
 #ifndef KNOWLEDGEITEM_H_
 #define KNOWLEDGEITEM_H_
 
+#include "storage_base.h"
+
 #include <ostream>
 #include <string>
 #include <ctime>
 
+class BaseStorageSaver;
+
 class KnowledgeItem {
 public:
-	KnowledgeItem(std::string title);
-	KnowledgeItem(std::string title, std::time_t creation_time);
-	virtual ~KnowledgeItem() = 0;
-	std::string title() const;
-	std::string creation_time_string() const;
-	std::time_t creation_time() const;
+    KnowledgeItem(std::string title);
+    KnowledgeItem(std::string title, std::time_t creation_time);
+    virtual ~KnowledgeItem() = 0;
+    std::string title() const;
+    std::string creation_time_string() const;
+    std::time_t creation_time() const;
 
-	virtual std::ostream& output(std::ostream& out);
-	template<typename StorageT>
-	void save_to(StorageT& storage);
+    virtual std::ostream& output(std::ostream& out);
+
+    virtual void save_to(BaseStorageSaver& storage);
 
 protected:
-	std::string _title;
-	std::time_t _creation_time;
+    std::string _title;
+    std::time_t _creation_time;
 };
 
 //typedef KnowledgeItem* KnowledgeItemPtr;
 using KnowledgeItemPtr = KnowledgeItem*;
 
-template<typename StorageT>
-inline void KnowledgeItem::save_to(StorageT& storage) {
-	//TODO: somehown implement as non-virtual method, possibly using a separate virtual method
-}
+
 
 #endif /* KNOWLEDGEITEM_H_ */
