32cbe08be911304c092b7ffd1c21efb68b339f13
Kostiantyn Zhereb <zhereb@gmail.com>
2022-11-03 15:39:36 +0200

    Improved tests for time_helper
    Currently some tests fail - both in time_helper and in
    StreamStorageLoader
    Need to resolve this (probably different implementation is needed)
    See #38

--- src/cpp/time_helper_test.cpp
+++ src/cpp/time_helper_test.cpp
@@ -16,9 +16,15 @@ TEST_CASE("get current time, convert to/from string") {
 	std::string current_str = time_to_string(current);
 	std::cout<<"current time is "<< current_str<<" ."<<std::endl;
 	//TODO: currently does not work, need either some better way of comparing times, or different implementations
-	//CHECK(time_from_string(current_str) == current);
+	CHECK(time_from_string(current_str) == current);
+
+	std::string converted = time_to_string(time_from_string(current_str));
+  std::cout<<"current time (converted) is "<< converted<<" ."<<std::endl;
 	//TODO: error seems to be exactly 1 hour - maybe check implementation
+	CHECK(converted == current_str);
+	//TODO: sometimes this check works, and previous doesn't, even though they should be the same
 	//CHECK(time_to_string(time_from_string(current_str)) == current_str);
+
 }
 
 
